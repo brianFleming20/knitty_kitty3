5725f80cb68520acde21efc336748f5c3ef5788f
Brian <brian.fleming03@outlook.com>
2023-01-29 12:19:26 +0000

    upadte images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -8,7 +9,10 @@ Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    x = datetime.now()
+
+    date = x.strftime("%A %d %B %Y")
+    return render_template("index.html", date=date)
 
 
 
