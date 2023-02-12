6bed33b36c47f01556fcce014f41889298829aa2
Brian <brian.fleming03@outlook.com>
2023-02-12 11:35:22 +0000

    adding sql

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from functools import wraps
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_ckeditor import CKEditor, CKEditorField
+from forms import CreatePostForm
 
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///knitty.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class KnittyItem(db.Model):
+    __tablename__ = "kitty_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+db.create_all()
+
 
 
 @app.route("/")
